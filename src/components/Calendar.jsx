af7c95a0a94b340d5d36825da5f9fe85337d7331
priyans619 <priyansh.2b@gmail.com>
2024-05-18 23:57:13 +0530

    thread for showing date

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import dayjs from 'dayjs';
 
 const Calendar = ({ currentDate }) => {
-  
+  if (!currentDate || !dayjs.isDayjs(currentDate)) {
+    return null; 
+  }
 
   const daysInMonth = currentDate.daysInMonth();
   const daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
@@ -10,7 +12,22 @@ const Calendar = ({ currentDate }) => {
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full table-fixed border-collapse border border-gray-300">
-        
+        <thead>
+          <tr>
+            {daysArray.map((day) => {
+              const date = currentDate.date(day);
+              return (
+                <th
+                  key={day}
+                  className="px-4 border border-gray-300 bg-gray-100 text-sm text-center"
+                >
+                  {date.format('D')}<br />
+                  {date.format('ddd')}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
         
       </table>
     </div>
