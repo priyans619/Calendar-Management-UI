370e3271ad89bd4574f461c7dbd4a23831b9859e
priyans619 <priyansh.2b@gmail.com>
2024-05-18 23:42:35 +0530

    import Card component with card visibility render

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import './App.css'
+import React, { useState } from 'react';
+import Card from './components/Card';
+import dayjs from 'dayjs';
 
 function App() {
+  const [isCardVisible, setIsCardVisible] = useState(false);
+  const [currentDate, setCurrentDate] = useState(dayjs());
+
+  const toggleCardVisibility = () => {
+    setIsCardVisible(!isCardVisible);
+  };
+
 
   return (
-    <>
-      <h1>Vite + React</h1>
-
-    </>
-  )
+    <div className="App">
+      <header className="App-header p-4 bg-blue-500 text-white text-center flex justify-between items-center">
+        <h1 className="text-2xl font-bold cursor-pointer" onClick={toggleCardVisibility}>
+          {currentDate.format('MMMM YYYY')}
+        </h1>
+        
+      </header>
+      {isCardVisible && <Card currentDate={currentDate} onClose={toggleCardVisibility} />}
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
