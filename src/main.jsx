40627462b4322b36945d02f7d428ea3ad92cfafa
priyans619 <priyansh.2b@gmail.com>
2024-05-19 22:34:13 +0530

    Update Create Root

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
+  </React.StrictMode>
+);
